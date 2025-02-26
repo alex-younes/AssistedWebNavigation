a083ebe94744f763c7e0c202774f144945517561
Alexandre Younes <younes.alexandre@outlook.com>
2025-02-26 10:54:25 +0200

    Update gitignore to handle node_modules better

--- backend/utils/debug.js
+++
@@ -1,9 +0,0 @@
-const { DEBUG } = require('../config/constants');
-
-const debug = (message, data = '') => {
-    if (DEBUG) {
-        console.log(`[DEBUG] ${message}`, data);
-    }
-};
-
-module.exports = debug;
